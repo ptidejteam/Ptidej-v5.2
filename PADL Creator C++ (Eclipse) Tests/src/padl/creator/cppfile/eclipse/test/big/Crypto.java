5fdabccc3297c7bd321530ca84919639339e453c
Yann-Gaël Guéhéneuc <yann-gael.gueheneuc@polymtl.ca>
2017-05-17 09:21:33 -0400

    Preparing to commit to GitHub
    Former-commit-id: c2f4e7f8fe9287cdd0316b0fe8fc3d852a51cf3f

---
+++ PADL Creator C++ (Eclipse) Tests/src/padl/creator/cppfile/eclipse/test/big/Crypto.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2001-2014 Yann-Gaël Guéhéneuc and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     Yann-Gaël Guéhéneuc and others, see in file; API and its implementation
+ ******************************************************************************/
+package padl.creator.cppfile.eclipse.test.big;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import padl.creator.cppfile.eclipse.misc.EclipseCPPParserCaller;
+import padl.kernel.ICodeLevelModel;
+
+public class Crypto extends TestCase {
+	public Crypto(String name) {
+		super(name);
+	}
+	public void test1() {
+		final ICodeLevelModel codeLevelModel =
+			EclipseCPPParserCaller
+				.getInstance()
+				.getCodeLevelModelUsingOSGiEmbedded(
+					"../PADL Creator C++ (Eclipse) Tests/data/crypto/");
+		Assert.assertNotNull("The code-level model is null!", codeLevelModel);
+		Assert.assertTrue(codeLevelModel.getNumberOfTopLevelEntities() > 5000);
+	}
+}
